7edc924491c82970e68eac762fa1e7eff24ca2a1
watanaka-yuichi <watanaka.yuichi.gx@mesw.co.jp>
2025-07-04 10:28:52 +0900

    #4376 [AIT] eval_llm_perplexity_score アラート対応

--- deploy/container/repository/my_ait.py
+++ deploy/container/repository/my_ait.py
@@ -89,12 +89,12 @@ if not is_ait_launch:
 
 
 if not is_ait_launch:
-    requirements_generator.add_package('pandas', '2.2.3')
-    requirements_generator.add_package('evaluate', '0.4.3')
-    requirements_generator.add_package('transformers', '4.46.3')
-    requirements_generator.add_package('torch', '2.5.1')
-    requirements_generator.add_package('torchvision', '0.20.1')
-    requirements_generator.add_package('torchaudio', '2.5.1')
+    requirements_generator.add_package('pandas', '2.3.0')
+    requirements_generator.add_package('evaluate', '0.4.4')
+    requirements_generator.add_package('transformers', '4.53.0')
+    requirements_generator.add_package('torch', '2.7.1')
+    requirements_generator.add_package('torchvision', '0.22.1')
+    requirements_generator.add_package('torchaudio', '2.7.1')
 
 
 # #### #3-3 [uneditable]
@@ -154,7 +154,7 @@ if not is_ait_launch:
     manifest_genenerator.set_ait_name('eval_llm_perplexity_score')
     manifest_genenerator.set_ait_description('LLMモデルで問題領域の質問に対して回答し、その生成されたテキストの品質を評価します。LLM評価基準を用いて、回答テキストのPerplexityスコアを計算し、テキストの質を数値化します。')
     manifest_genenerator.set_ait_source_repository('https://github.com/aistairc/Qunomon_AIT_Repository/tree/eval_llm_perplexity_score')
-    manifest_genenerator.set_ait_version('1.0')
+    manifest_genenerator.set_ait_version('1.1')
     manifest_genenerator.add_ait_licenses('Apache License Version 2.0')
     manifest_genenerator.add_ait_keywords('LLM')
     manifest_genenerator.add_ait_keywords('Perplexity')
